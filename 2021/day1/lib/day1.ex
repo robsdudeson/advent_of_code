6051fe2965e5cd5b9bd08d8b613a572fd4486d12
Robby Thompson <robsdudeson@gmail.com>
2021-12-13 12:21:20 -0500

    day 1 part 2

--- 2021/day1/lib/day1.ex
+++ 2021/day1/lib/day1.ex
@@ -5,12 +5,20 @@ defmodule Day1 do
 
   def part1(file_path) do
     file_path
-    |> File.read!()
-    |> String.trim()
-    |> String.split("\n")
-    |> Enum.map(fn reading -> Integer.parse(reading) end)
+    |> get_readings()
     |> count_greater_than()
   end
+
+  defp get_readings(file_path),
+    do:
+      file_path
+      |> File.read!()
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.map(fn reading ->
+        {reading_int, _} = Integer.parse(reading)
+        reading_int
+      end)
 
   defp count_greater_than(readings, previous \\ nil, count \\ 0)
 
@@ -26,4 +34,22 @@ defmodule Day1 do
 
     count_greater_than(tail, current, new_count)
   end
+
+  def part2(file_path) do
+    file_path
+    |> get_readings()
+    |> sliding_window()
+  end
+
+  defp sliding_window(readings, previous_sum \\ nil, count \\ 0)
+
+  defp sliding_window([], _previous_sum, count), do: count
+
+  defp sliding_window([a, b, c | tail], previous_sum, count) do
+    new_sum = a + b + c
+    count = if previous_sum && new_sum > previous_sum, do: count + 1, else: count
+    sliding_window([b, c] ++ tail, new_sum, count)
+  end
+
+  defp sliding_window(_, _, count), do: count
 end
--- 2021/day1/test/day1_test.exs
+++ 2021/day1/test/day1_test.exs
@@ -12,4 +12,14 @@ defmodule Day1Test do
       assert Day1.part1("#{@input_dir}/input_full") == 1195
     end
   end
+
+  describe "Day1.part2 - counts when a sliding window count increases" do
+    test "sample input" do
+      assert Day1.part2("#{@input_dir}/input_sample") == 5
+    end
+
+    test "full input" do
+      assert Day1.part2("#{@input_dir}/input_full") == 1235
+    end
+  end
 end
